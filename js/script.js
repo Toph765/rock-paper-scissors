f434ca8c5bbd8966ebd3534e1f2590b71989e5c5
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2023-06-23 20:21:15 +0800

    Add score system and finalResult()

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const choices = ["rock", "paper", "scissors"];
 const buttons = document.querySelectorAll('button');
 const result = document.querySelector('.result');
+const score = document.querySelector('.score');
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
@@ -16,9 +19,13 @@ function playRound(playerSelection, computerSelection) {
                playerSelection === "paper" && computerSelection === "rock" ||
                playerSelection === "scissors" && computerSelection === "paper") {
         message = `You won! ${playerSelection} beats ${computerSelection}!`;
+        playerScore++;
+        console.log('player:' + playerScore)
         return message;
     } else {
         message = `You lost! ${computerSelection} beats ${playerSelection}!`;
+        computerScore++;
+        console.log('computer:' + computerScore)
         return message;
     }
 } 
@@ -27,4 +34,18 @@ buttons.forEach(button => button.addEventListener('click', () => {
     const playerSelection = button.textContent.toLowerCase();
     const computerSelection = getComputerChoice();
     result.textContent = playRound(playerSelection, computerSelection);
+    score.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+    if (playerScore === 5 || computerScore === 5) {
+        finalResult()
+        playerScore = 0;
+        computerScore = 0;
+    }
 }))
+
+function finalResult () {
+    if (playerScore > computerScore) {
+        return result.textContent = 'Congratulations! You won!';
+    } else {
+        return result.textContent = 'Oh shucks! You lose!';
+    }
+}
